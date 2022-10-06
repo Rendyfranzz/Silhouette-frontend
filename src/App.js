6ecffe22623dc96bebee3bb56182e89b07e7fc98
Rendi <rendicrew96@gmail.com>
2022-10-06 22:05:09 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import About from './layout/About';
+import BookOnline from './layout/BookOnline';
 import Home from './layout/Home';
 import PageNotFound from './layout/PageNotFound';
 import PriceList from './layout/PriceList';
@@ -15,7 +16,8 @@ function App() {
         <Route path='/Home' element={<Home/>}/> 
         <Route path='/About' element={<About/>}/>
         <Route path='/PriceList' element={<PriceList/>}/>
-        <Route path='/*' element={<PageNotFound/>}/>
+        <Route path='/BookOnline' element={<BookOnline/>}/>
+        <Route path='*' element={<PageNotFound/>}/>
       </Routes>
       <Footer/>
     </Router>
---
+++ src/layout/BookOnline.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BookOnline = () => {
+  return (
+    <div className='min-h-screen'>BookOnline</div>
+  )
+}
+
+export default BookOnline
