c27838329bb919ace724a1e5ed0855453fd19cde
Rendi <rendicrew96@gmail.com>
2022-11-20 23:15:47 +0700

    update

--- src/pages/ListOrder.js
+++ src/pages/ListOrder.js
@@ -1,26 +1,139 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
 import AdminLayout from './AdminLayout'
-import { useDispatch,useSelector } from "react-redux"
-import { useNavigate } from "react-router-dom"
-import { getMe } from '../feature/authSlice'
+import axios from 'axios'
+
 const ListOrder = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const {isError}= useSelector((state)=> state.auth)
+  const [users,setUsers] = useState([])
 
   useEffect(()=>{
-    dispatch(getMe());
-  },[dispatch])
+    getUsers();
+  },[])
 
-  useEffect(()=>{
-    if (isError){
-      navigate("/login")
-    }
-  },[isError,navigate])
+  const getUsers = async () =>{
+    const response = await axios.get("http://localhost:5000/users");
+    setUsers(response.data)
+  }
 
+  const deleteUser = async (userId) =>{
+    await axios.delete(`http://localhost:5000/users/${userId}`)
+    getUsers()
+  }
   return (
    <AdminLayout>
-    List order ngab
+     <div className="mt-20 min-h-screen">
+            <div className="flex flex-col">
+                <div className="overflow-x-auto w-full">
+                    <div className="py-3 pl-2">
+                        <div className="relative max-w-xs">
+                            <label htmlFor="hs-table-search" className="sr-only">
+                                Search
+                            </label>
+                            <input
+                                type="text"
+                                name="hs-table-search"
+                                id="hs-table-search"
+                                className="block w-full p-3 pl-10 text-sm border-gray-200 rounded-md focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"
+                                placeholder="Search..."
+                            />
+                            <div className="absolute inset-y-0 left-0 flex items-center pl-4 pointer-events-none">
+                                <svg
+                                    className="h-3.5 w-3.5 text-gray-400"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="16"
+                                    height="16"
+                                    fill="currentColor"
+                                    viewBox="0 0 16 16"
+                                >
+                                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="p-1.5 w-full inline-block align-middle">
+                        <div className="overflow-hidden border rounded-lg">
+                            <table className="min-w-full divide-y divide-gray-200">
+                                <thead className="bg-gray-50">
+                                    <tr>
+                                        <th
+                                            scope="col"
+                                            className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
+                                        >
+                                            ID
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
+                                        >
+                                            Name
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
+                                        >
+                                            Email
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
+                                        >
+                                            Role
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-6 py-3 text-xs font-bold text-right text-gray-500 uppercase "
+                                        >
+                                            Edit
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-6 py-3 text-xs font-bold text-right text-gray-500 uppercase "
+                                        >
+                                            Delete
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody className="divide-y divide-gray-200">
+                                  {
+                                    users.map((data,index)=>(
+                                      <tr key={data.uuid}>
+                                        <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
+                                            {index+1}
+                                        </td>
+                                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
+                                            {data.name}
+                                        </td>
+                                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
+                                            {data.email}
+                                        </td>
+                                        <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
+                                            {data.role}
+                                        </td>
+                                        <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
+                                            <button
+                                                className="text-green-500 hover:text-green-700"
+                                            >
+                                                Edit
+                                            </button>
+                                        </td>
+                                        <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
+                                            <button onClick={()=>deleteUser(data.uuid)}
+                                                className="text-red-500 hover:text-red-700"
+                                            >
+                                                Delete
+                                            </button>
+                                        </td>
+                                    </tr>
+                                    ))
+                                  }
+                                    
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
    </AdminLayout>
   )
 }
