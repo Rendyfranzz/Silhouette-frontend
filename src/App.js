9cca0f6aa797fca1660856e1e9107b969220faf7
Rendi <rendicrew96@gmail.com>
2022-12-02 16:13:21 +0700

    update

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import ListUser from './pages/ListUser';
 import ListOrder from './pages/ListOrder';
 import Payment from './pages/Payment';
 import { AnimatePresence } from 'framer-motion';
+import UserPayment from './pages/UserPayment';
 function App() {
   let location = useLocation()
  
@@ -20,17 +21,18 @@ function App() {
       <AnimatePresence node='wait'>
       <Routes key={location.pathname} location={location}>
         <Route path='/' element={<Home/>}/>
-        <Route path='/Home' element={<Home/>}/> 
-        <Route path='/About' element={<About/>}/>
-        <Route path='/PriceList' element={<PriceList/>}/>
-        <Route path='/BookOnline' element={<BookOnline/>}/>
-        <Route path='/Login' element={<Login/>}/>
+        <Route path='/home' element={<Home/>}/> 
+        <Route path='/about' element={<About/>}/>
+        <Route path='/priceList' element={<PriceList/>}/>
+        <Route path='/bookOnline' element={<BookOnline/>}/>
+        <Route path='/login' element={<Login/>}/>
         <Route path='/register' element={<Register/>}/>
         <Route path='/admin' element={<Admin/>}/>
-        <Route path='/booknext' element={<BookNext/>}/>
+        <Route path='/booknext' element={<BookNext/>}/> 
         <Route path='/listuser' element={<ListUser/>}/>
         <Route path='/listorder' element={<ListOrder/>}/>
         <Route path='/payment' element={<Payment/>}/>
+        <Route path='/userdetail/:uuid' element={<UserPayment/>}/>
         <Route path='*' element={<PageNotFound/>}/>
       </Routes>
       </AnimatePresence>
