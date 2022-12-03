ca2d010f018231610339d60dab072d178b39df4a
Rendi <rendicrew96@gmail.com>
2022-12-03 23:32:39 +0700

    update

--- src/pages/About.js
+++ src/pages/About.js
@@ -18,15 +18,15 @@ const About = () => {
     <AnimatedPage>
     <><Navbar/>
    
-    <div className='min-h-screen flex flex-col justify-center items-center font-popin'>
+    <div className='min-h-screen flex flex-col justify-center items-center text-black'>
       <div className="w-screen h-48 flex flex-col items-center justify-center p-2 overflow-hidden">
-        <h1 className='text-title font-bold'>About</h1>
+        <h1 className='font-bold'>About</h1>
         <p> lorem</p>
       </div>
 
       <div className='w-screen h-48 bg-black p-2 overflow-hidden'>
-        <h1 className=' text-lg md:text-title text-white text-center font-bold'>WHY BOOK WITH US?</h1>
-        <div className='text-xs md:text-sm flex flex-row justify-center items-center space-x-6 p-4'>
+        <h1 className=' text-white text-center font-bold'>WHY BOOK WITH US?</h1>
+        <div className='flex flex-row justify-center items-center space-x-6 p-4'>
           <CardAbout><FaRegSmile size={60} /><p>No Deposit!</p></CardAbout>
           <CardAbout><FaCheckCircle size={60} />Quality Approved!</CardAbout>
           <CardAbout><FaRegTimesCircle size={60} />No Cancellation Penalty!</CardAbout>
@@ -35,9 +35,9 @@ const About = () => {
       </div>
 
       <div className='w-screen h-48 flex flex-col md:flex-row justify-evenly items-center overflow-hidden'>
-        <h1 className=' text-lg md:text-title text-black text-center font-bold'>Jam Operasional</h1>
+        <h1 className='text-black text-center font-bold'>Jam Operasional</h1>
         <div className='flex flex-row justify-center items-center space-x-6'>
-          <table className='border-separate border-spacing-2 border-spacing-x-8 text-sm md:text-lg'>
+          <table className='border-separate border-spacing-2 border-spacing-x-8'>
             <tbody>
             <tr>
               <td>Sabtu-Rabu</td>
@@ -57,7 +57,7 @@ const About = () => {
       </div>
 
       <div className="w-screen flex justify-center items-center flex-col space-y-4">
-      <h1 className=' text-lg md:text-title text-black text-center font-bold'>Lokasi</h1>
+      <h1 className='text-black text-center font-bold'>Lokasi</h1>
           <Map/>
       </div>
     </div>
